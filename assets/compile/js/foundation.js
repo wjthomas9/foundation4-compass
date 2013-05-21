90ca5bf6497f120c875b5cb6dbbf6804072d36b6
Jon Thomas <jon@clearfirestudios.com>
2013-05-21 18:10:29 -0500

    Fix guard juicer processes

---
+++ assets/compile/js/foundation.js
@@ -0,0 +1,16 @@
+/**
+* @depend foundation/foundation.js
+* @depend foundation/foundation.alerts.js
+* @depend foundation/foundation.clearing.js
+* @depend foundation/foundation.cookie.js
+* @depend foundation/foundation.dropdown.js
+* @depend foundation/foundation.forms.js
+* @depend foundation/foundation.joyride.js
+* @depend foundation/foundation.magellan.js
+* @depend foundation/foundation.orbit.js
+* @depend foundation/foundation.placeholder.js
+* @depend foundation/foundation.reveal.js
+* @depend foundation/foundation.section.js
+* @depend foundation/foundation.tooltips.js
+* @depend foundation/foundation.topbar.js
+*/
